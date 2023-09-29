288b29cbf965d612a5d01e31e2fec8780c52be24
Alex_laptop <aleksandriliev05@gmail.com>
2023-09-29 21:22:56 +0300

    Changed draw module Draw.__init__() no longer takes arguments Draw.draw_board() now takes Board as argument Updated other files with the new implementation Added description comments in draw.py

--- game.py
+++ game.py
@@ -40,10 +40,9 @@ class Game_object:
             
             # draw.draw_board(self.board) 
             self.screen.fill("purple")
-            d = Draw(self.board.get_board(), self.board.get_selected(), self.board.get_highlighted())
-            d.draw_board(self.screen)
-            pygame.display.flip()
-            
+            draw = Draw()
+            draw.draw_board(self.screen, self.board)
+            pygame.display.flip()            
 
             self.clock.tick(60)  # limits FPS to 60
         pygame.quit()
